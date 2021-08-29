772a0c6a962791feccd6b85ecc29a37aa1938a9e
manot40 <kevinsandiho@outlook.com>
2021-08-29 20:57:34 +0700

    master

---
+++ components/FilterableDropdown.vue
@@ -0,0 +1,154 @@
+<template>
+  <div ref="wrapper" class="dropdown w-full relative fd__wrapper">
+    <input type="hidden" :name="name" :value="value" />
+    <input
+      ref="textBox"
+      type="text"
+      class="input input-bordered w-56 sm:w-full md:w-full lg:w-full xl:w-full 2xl:w-full fd__display-textbox"
+      :disabled="disabled"
+      :value="displayText"
+      :placeholder="placeholder"
+      readonly="readonly"
+      @click="textBoxClicked"
+    />
+    <div
+      class="absolute z-30 top-full -left-2 p-2 -mt-px w-full"
+      v-show="showList"
+    >
+      <ul
+        class="
+          p-2
+          shadow
+          menu
+          dropdown-content
+          bg-base-200
+          rounded-box
+          w-full
+          max-h-48
+          overflow-x-hidden overflow-y-scroll
+          z-10
+        "
+        tabindex="0"
+        v-if="filteredItems.length > 0"
+      >
+        <div class="fd__filter-input">
+          <input
+            ref="filterTextBox"
+            type="text"
+            class="input input-sm w-full"
+            placeholder="Cari Entry..."
+            v-model="filterString"
+          />
+        </div>
+        <li
+          class="
+            fd__item
+            p-4
+            mt-2
+            leading-none
+            hover:bg-base-300
+            rounded-xl
+            cursor-pointer
+          "
+          v-for="item in filteredItems"
+          :key="'item_' + (idKey ? item[idKey] : item)"
+          :value="valueKey ? item[valueKey] : item"
+          @click.stop.prevent="itemClicked(item)"
+        >
+          {{ textKey ? item[textKey] : item }}
+        </li>
+        <li v-if="filteredItems.length === 0">No Items</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      filterString: null,
+      showList: false,
+    };
+  },
+  props: {
+    name: { type: String, default: "" },
+    value: { type: [String, Number, Boolean, Date], default: null },
+    placeholder: { type: String, default: "" },
+    disabled: { type: Boolean, default: false },
+    showEmptyItem: { type: Boolean, default: false },
+    emptyItemText: { type: String, default: null },
+    emptyItemValue: { type: String, default: null },
+    items: { type: Array, default: null },
+    idKey: { type: String, default: null },
+    valueKey: { type: String, default: null },
+    textKey: { type: String, default: null },
+    filterTargetKey: { type: String, default: null },
+    ignoreCase: { type: Boolean, default: true },
+  },
+  computed: {
+    filteredItems() {
+      if (!this.filterString) return this.items;
+      if (!this.items || this.items.length === 0) return [];
+      const flags = this.ignoreCase ? "i" : "";
+      const regexp = new RegExp(this.filterString, flags);
+      return this.items.filter((x) => {
+        const targetValue = this.filterTargetKey
+          ? x[this.filterTargetKey]
+          : JSON.stringify(x);
+        return regexp.test(targetValue);
+      });
+    },
+    selectedItem() {
+      return this.items.find(
+        (x) => this.value === (this.valueKey ? x[this.valueKey] : x)
+      );
+    },
+    displayText() {
+      const item = this.selectedItem;
+      if (!item) return "";
+      return this.textKey ? item[this.textKey] : item;
+    },
+  },
+  methods: {
+    textBoxClicked() {
+      if (this.disabled) return;
+      this.showList = !this.showList;
+    },
+    itemClicked(item) {
+      if (item) {
+        const value = this.valueKey ? item[this.valueKey] : item;
+        this.$emit("input", value);
+      } else {
+        this.$emit("input", this.emptyItemValue || null);
+      }
+      this.reset();
+    },
+    reset() {
+      this.showList = false;
+      this.filterString = "";
+    },
+  },
+  watch: {
+    showList(val) {
+      this.$nextTick(() => {
+        if (val) {
+          try {
+            this.$refs.filterTextBox.focus();
+          } catch {
+              this.$toast.info('Harap pilih gudang terlebih dahulu');
+          }
+        }
+      });
+    },
+  },
+  mounted() {
+    const $this = this;
+    document.addEventListener("click", function (e) {
+      const target = (e.target || e.srcElement).closest(".fd__wrapper");
+      if (target === $this.$refs.wrapper) return;
+      $this.reset();
+    });
+  },
+};
+</script>
--- pages/input.vue
+++ pages/input.vue
@@ -5,14 +5,37 @@
         INPUT TRANSAKSI BARU
       </h1>
       <p class="mb-8 text-left font-light antialiased tracking-wider">
-        Upload file excel yang berisikan list data untuk di impor ke database
+        Masukan detail transaksi pada form dan tambah item pada tabel yang
+        tersedia
       </p>
       <div class="flex xl:w-1/2 2xl:w-1/2">
         <div class="form-control mr-4">
           <label class="label">
             <span class="label-text">Tanggal Transaksi</span>
           </label>
-          <input type="date" v-model="form.txDate" class="input input-bordered w-full">
+          <input
+            type="date"
+            v-model="form.txDate"
+            class="input input-bordered w-full"
+          />
+        </div>
+        <div class="form-control mr-4">
+          <label class="label">
+            <span class="label-text">Pilih Gudang</span>
+          </label>
+          <select
+            class="select select-bordered w-full max-w-xs"
+            v-model="form.warehouse"
+            @change="getItemsData()"
+          >
+            <option
+              v-for="(item, index) in preload.warehouse"
+              :key="index"
+              :value="item._id"
+            >
+              {{ item.name }}
+            </option>
+          </select>
         </div>
       </div>
       <div class="flex mb-8 xl:w-1/2 2xl:w-1/2">
@@ -20,28 +43,28 @@
           <label class="label">
             <span class="label-text">Deskripsi</span>
           </label>
-          <input type="text" placeholder="Masukan Deskripsi" class="input input-bordered" v-model="form.description">
+          <input
+            type="text"
+            placeholder="Masukan Deskripsi"
+            class="input input-bordered"
+            v-model="form.description"
+          />
         </div>
         <div class="form-control mr-4">
           <label class="label">
             <span class="label-text">Jenis</span>
           </label>
-          <select class="select select-bordered w-full max-w-xs" v-model="form.type">
-            <option selected="selected" value="out">Keluar</option> 
-            <option selected="selected" value="in">Masuk</option> 
-          </select>
-        </div>
-        <div class="form-control mr-4">
-          <label class="label">
-            <span class="label-text">Pilih Gudang</span>
-          </label>
-          <select class="select select-bordered w-full max-w-xs" v-model="form.warehouse" @change="getItemsData()">
-            <option v-for="(item, index) in preload.warehouse" :key="index" :value="item._id">{{ item.name }}</option>
+          <select
+            class="select select-bordered w-full max-w-xs"
+            v-model="form.type"
+          >
+            <option selected="selected" value="out">Keluar</option>
+            <option selected="selected" value="in">Masuk</option>
           </select>
         </div>
       </div>
       <div class="overflow-x-auto max-h-96">
-        <table class="table w-full">
+        <table class="table">
           <thead>
             <tr>
               <th></th>
@@ -58,16 +81,42 @@
             <tr v-for="(item, index) in form.items" :key="index">
               <th scope="row" />
               <td>
-                <select class="select select-bordered w-48 sm:w-full md:w-full lg:w-full xl:w-full 2xl:w-full" v-model="form.items[index].item">
-                  <option v-for="(item, index) in preload.items" :key="index" :value="item._id">{{ item.name }}</option>
-                </select>
-                <!-- <input type="text" placeholder="Item name" class="input input-bordered sm:w-full md:w-full lg:w-full xl:w-full 2xl:w-full" v-model.trim="form.items[index].name" :key="index"> -->
-              </td>
-              <td class="w-48 mb-8">
-                <input type="number" placeholder="Quantity" class="input input-bordered" v-model.trim="form.items[index].quantity" :key="index">
+                <FilterableDropdown
+                  v-model="form.items[index].item"
+                  :items="preload.items"
+                  :showEmptyItem="true"
+                  :disabled="false"
+                  :ignoreCase="true"
+                  placeholder="Pilih Item"
+                  idKey="_id"
+                  valueKey="_id"
+                  textKey="name"
+                  filterTargetKey="name"
+                />
               </td>
               <td>
-                <button v-if="form.items.length>1" class="btn btn-sm btn-error" @click="rmTblData(index)">
+                <input
+                  type="number"
+                  placeholder="Quantity"
+                  class="
+                    input input-bordered
+                    w-28
+                    sm:w-full
+                    md:w-full
+                    lg:w-full
+                    xl:w-full
+                    2xl:w-full
+                  "
+                  v-model.trim="form.items[index].quantity"
+                  :key="index"
+                />
+              </td>
+              <td>
+                <button
+                  v-if="form.items.length > 1"
+                  class="btn btn-sm btn-error"
+                  @click="rmTblData(index)"
+                >
                   <ion-icon class="text-lg" name="trash-sharp"></ion-icon>
                 </button>
               </td>
@@ -76,9 +125,9 @@
           <tfoot>
             <tr>
               <th></th>
-              <th class="w-1/2">Item Name</th>
-              <th class="w-1/4">Quantity</th>
-              <th class="w-1/4">
+              <th>Item Name</th>
+              <th>Quantity</th>
+              <th>
                 <button class="btn btn-info btn-sm">
                   <p class="font-bold" @click="addTblData()">Tambah Baris</p>
                 </button>
@@ -100,86 +149,89 @@
 </template>
 
 <script>
-import dayjs from 'dayjs';
+import FilterableDropdown from "../components/FilterableDropdown.vue";
+import dayjs from "dayjs";
 
 export default {
   beforeMount() {},
   mounted() {
-      this.getWarehouseData();
+    this.getWarehouseData();
+  },
+  components: {
+    FilterableDropdown,
   },
   data() {
     return {
       isLoading: false,
       preload: {
-          warehouse: [],
-          items: []
+        warehouse: [],
+        items: [],
       },
       form: {
-        txDate: dayjs().format('YYYY-MM-DD'),
-        description: dayjs().format('[Transaksi] DD MMMM'),
-        type: 'out',
+        txDate: dayjs().format("YYYY-MM-DD"),
+        description: dayjs().format("[Transaksi] DD MMMM"),
+        type: "out",
         warehouse: "",
         items: [
-            {item: '', quantity: ''},
-            {item: '', quantity: ''},
-            {item: '', quantity: ''},
+          { item: "", quantity: "" },
+          { item: "", quantity: "" },
+          { item: "", quantity: "" },
         ],
       },
     };
   },
   methods: {
     rmTblData(item) {
-      console.log(this.form.txDate);
       this.form.items.splice(item, 1);
     },
     addTblData() {
-      this.form.items.push({item: '', quantity: ''});
+      this.form.items.push({ item: "", quantity: "" });
     },
     async getWarehouseData() {
-        const response = await this.$axios
-          .$get("/warehouse")
-          .then(function(res) {
-              return res;
-          })
-          .catch(function(err) {
-              console.log(err);
-              return false;
-          })
-        if(response) {
-            this.preload.warehouse = response;
-        }
+      const response = await this.$axios
+        .$get("/warehouse")
+        .then(function (res) {
+          return res;
+        })
+        .catch(function (err) {
+          console.log(err);
+          return false;
+        });
+      if (response) {
+        this.preload.warehouse = response;
+      }
     },
     async getItemsData() {
-        const warehouse = this.form.warehouse;
-        const response = await this.$axios
-          .$get(`/warehouse/${warehouse}?itemOnly=true`)
-          .then(function(res) {
-              return res.items;
-          })
-          .catch(function(err) {
-              console.log(err);
-              return false;
-          })
-        if(response) {
-            this.preload.items = response;
-        }
+      const warehouse = this.form.warehouse;
+      const response = await this.$axios
+        .$get(`/warehouse/${warehouse}?itemOnly=true`)
+        .then(function (res) {
+          return res.items;
+        })
+        .catch(function (err) {
+          console.log(err);
+          return false;
+        });
+      if (response) {
+        this.preload.items = response;
+      }
     },
     async beginSubmit() {
       this.isLoading = true;
       const data = {
-          txDate: dayjs(this.form.txDate).format('YYYY-MM-DDThh:mm:ss.SSSZ'),
-          description: this.form.description,
-          type: this.form.type,
-          warehouse: this.form.warehouse,
-          items: this.form.items,
-      }
+        txDate: dayjs(this.form.txDate).format("YYYY-MM-DDThh:mm:ss.SSSZ"),
+        description: this.form.description,
+        type: this.form.type,
+        warehouse: this.form.warehouse,
+        items: this.form.items,
+      };
       const response = await this.$axios
         .$post("/transaction", {
-            txDate: data.txDate,
-            description: data.description,
-            type: data.type,
-            warehouse: data.warehouse,
-            items: data.items,
+          txDate: data.txDate,
+          description: data.description,
+          type: data.type,
+          warehouse: data.warehouse,
+          items: data.items,
         })
         .then(function (res) {
           return true;
@@ -187,16 +239,12 @@ export default {
         .catch(function (err) {
           return false;
         });
-      if(response) {
+      if (response) {
         this.isLoading = false;
-        this.$toast.success(
-          "Submit Berhasil"
-        );
+        this.$toast.success("Submit Berhasil");
       } else {
         this.isLoading = false;
-        this.$toast.error(
-          "Submit Gagal."
-        );
+        this.$toast.error("Submit Gagal.");
       }
     },
   },
