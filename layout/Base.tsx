453dda6b9e48a858bd2ce1d80c3eb7d0ffac0ae7
manot40 <kevinsandiho@outlook.com>
2022-01-09 14:08:44 +0700

    master

--- layout/Base.tsx
+++ layout/Base.tsx
@@ -2,6 +2,7 @@
 import AuthProvider from 'libs/context/AuthContext'
 import { HTMLAttributes } from "react";
 import { useTheme } from "next-themes";
+import { SWRConfig } from 'swr';
 import Head from "next/head";
 import { FC } from "react";
 
@@ -14,9 +15,9 @@ export const BaseLayout: FC<Props> = ({ children }) => {
       <Head>
         <meta name="theme-color" content={theme === "dark" ? "#000000" : "#FFFFFF"} />
       </Head>
-      <AuthProvider>
-        {children}
-      </AuthProvider>
+      <SWRConfig value={{ errorRetryInterval: 300, errorRetryCount: 20 }}>
+        <AuthProvider>{children}</AuthProvider>
+      </SWRConfig>
     </div>
   );
 };
